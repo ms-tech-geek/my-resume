beb4a2f5b3aa2bc6aae03f16310761681d6bfe9c
Mayank Sethi <mayank@digibrain.co.in>
2024-05-13 16:51:19 +0530

    decompose experience card component into smaller components like companyLogo, jobDetails and jobDurationLocation

--- components/experienceCard.jsx
+++ components/experienceCard.jsx
@@ -1,23 +1,31 @@
 import styles from '@/components/styles.module.scss';
 
+const CompanyLogo = ({ logo, companyName }) => (
+  <div className={`col-1 ${styles.companyLogoContainer}`}>
+    <img src={`./images/${logo}.png`} alt={`${companyName} logo`} className={styles.companyLogo} />
+  </div>
+);
+
+const JobDetails = ({ jobTitle, companyName }) => (
+   <div className={`col-7 ${styles.jobDetails}`}>
+    <div className={styles.jobTitle}>{jobTitle}</div>
+    <div className={styles.companyName}>{companyName}</div>
+  </div>
+);
+
+const JobDurationLocation = ({ jobDuration, companyLocation }) => (
+  <div className={`col-4 ${styles.jobDurationLocation}`}>
+    <div className={styles.jobDuration}>{jobDuration}</div>
+    <div className={styles.companyLocation}>{companyLocation}</div>
+  </div>
+);
+
 const ExperienceCard = ({ expDetails: { companyInfo, jobInfo } }) => {
   return (
     <div className={`row mt-2 ${styles.expCardBox}`}>
-      <div className={`col-1 ${styles.companyLogoContainer}`}>
-        <img
-          src={`./images/${companyInfo.companyLogo}.png`}
-          alt={`${companyInfo.companyName} logo`}
-          className={styles.companyLogo}
-        />
-      </div>
-      <div className={`col-7 ${styles.jobDetails}`}>
-        <div className={styles.jobTitle}>{jobInfo.jobTitle}</div>
-        <div className={styles.companyName}>{companyInfo.companyName}</div>
-      </div>
-      <div className={`col-4 ${styles.jobDurationLocation}`}>
-        <div className={styles.jobDuration}>{jobInfo.jobDuration}</div>
-        <div className={styles.companyLocation}>{companyInfo.companyLocation}</div>
-      </div>
+      <CompanyLogo logo={companyInfo.companyLogo} companyName={companyInfo.companyName} />
+      <JobDetails jobTitle={jobInfo.jobTitle} companyName={companyInfo.companyName}/>
+      <JobDurationLocation jobDuration={jobInfo.jobDuration} companyLocation={companyInfo.companyLocation}/>
     </div>
   );
 };
