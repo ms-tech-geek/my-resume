71953c4d108d98815620225f515e941bc7500181
Mayank Sethi <mayank@digibrain.co.in>
2024-05-13 19:58:09 +0530

    added location icon and refined styling for job duration and location

--- components/experienceCard.jsx
+++ components/experienceCard.jsx
@@ -1,34 +1,55 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import styles from '@/components/styles.module.scss';
 
 const CompanyLogo = ({ logo, companyName }) => {
   const logoPath = `./images/${logo}.png`;
   const logoText = `${companyName} logo`;
-  return(
-  <div className={`col-1 ${styles.companyLogoContainer}`}>
-    <img src={logoPath} alt={logoText} className={styles.companyLogo} />
-  </div>)
+  return (
+    <div className={`col-1 ${styles.companyLogoContainer}`}>
+      <img src={logoPath} alt={logoText} className={styles.companyLogo} />
+    </div>
+  );
 };
 
 const JobDetails = ({ jobTitle, companyName }) => (
-   <div className={`col-7 ${styles.jobDetails}`}>
+  <div className={`col-6 ${styles.jobDetails}`}>
     <div className={styles.jobTitle}>{jobTitle}</div>
     <div className={styles.companyName}>{companyName}</div>
   </div>
 );
 
 const JobDurationLocation = ({ jobDuration, companyLocation }) => (
-  <div className={`col-4 ${styles.jobDurationLocation}`}>
-    <div className={`${styles.jobDuration} ${jobDuration === 'Present' ? styles.activeJob : ''}`}>{jobDuration}</div>
-    <div className={styles.companyLocation}>{companyLocation}</div>
+  <div className={`col-5 ${styles.jobDurationLocation}`}>
+    <div
+      className={`${styles.jobDuration} ${
+        jobDuration === 'Present' ? styles.activeJob : ''
+      }`}
+    >
+      {jobDuration}
+    </div>
+    <div className={styles.companyLocation}>
+      <FontAwesomeIcon icon={faLocationDot} className={styles.locationIcon} />{' '}
+      {companyLocation}
+    </div>
   </div>
 );
 
-const ExperienceCard = ({ companyLogo, companyName, companyLocation, jobTitle, jobDuration}) => {
+const ExperienceCard = ({
+  companyLogo,
+  companyName,
+  companyLocation,
+  jobTitle,
+  jobDuration,
+}) => {
   return (
     <div className={`row mt-2 ${styles.expCardBox}`}>
       <CompanyLogo logo={companyLogo} companyName={companyName} />
-      <JobDetails jobTitle={jobTitle} companyName={companyName}/>
-      <JobDurationLocation jobDuration={jobDuration} companyLocation={companyLocation}/>
+      <JobDetails jobTitle={jobTitle} companyName={companyName} />
+      <JobDurationLocation
+        jobDuration={jobDuration}
+        companyLocation={companyLocation}
+      />
     </div>
   );
 };
