dc2c932dacf7e499c0923a3042170ffde9afa4ac
Mayank Sethi <mayank@digibrain.co.in>
2024-05-13 20:20:52 +0530

    styling added to experience section

--- components/experienceSection.jsx
+++ components/experienceSection.jsx
@@ -1,40 +1,41 @@
-import ExperienceCard from './experienceCard';
+import ExperienceCard from '@/components/experienceCard';
+import styles from '@/components/styles.module.scss';
 
 const experienceInfo = [
   {
-      companyLogo: 'meta-logo',
-      companyName: 'Meta',
-      companyLocation: 'Menlo Park, California',
-      jobTitle: 'VR designer',
-      jobDuration: 'Present',
+    companyLogo: 'meta-logo',
+    companyName: 'Meta',
+    companyLocation: 'Menlo Park, California',
+    jobTitle: 'VR designer',
+    jobDuration: 'Present',
   },
   {
-      companyLogo: 'apple-logo',
-      companyName: 'Apple',
-      companyLocation: 'Cupertino, California',
-      jobTitle: 'Product designer',
-      jobDuration: 'Jul 20 - Jan 2022',
+    companyLogo: 'apple-logo',
+    companyName: 'Apple',
+    companyLocation: 'Cupertino, California',
+    jobTitle: 'Product designer',
+    jobDuration: 'Jul 20 - Jan 2022',
   },
   {
-      companyLogo: 'tesla-logo',
-      companyName: 'Tesla',
-      companyLocation: 'Austin, Texas',
-      jobTitle: 'UX Designer',
-      jobDuration: 'Oct 2015 - Mar 2020',
+    companyLogo: 'tesla-logo',
+    companyName: 'Tesla',
+    companyLocation: 'Austin, Texas',
+    jobTitle: 'UX Designer',
+    jobDuration: 'Oct 2015 - Mar 2020',
   },
   {
-      companyLogo: 'google-logo',
-      companyName: 'Google',
-      companyLocation: 'Mountain View, California',
-      jobTitle: 'Design system architect',
-      jobDuration: 'Sep 2014 - Aug 2015',
+    companyLogo: 'google-logo',
+    companyName: 'Google',
+    companyLocation: 'Mountain View, California',
+    jobTitle: 'Design system architect',
+    jobDuration: 'Sep 2014 - Aug 2015',
   },
   {
-      companyLogo: 'vectornator-logo',
-      companyName: 'Vectornator',
-      companyLocation: 'Berlin, Germany',
-      jobTitle: 'Senior product design',
-      jobDuration: 'Sep 2010 - Jul 2013',
+    companyLogo: 'vectornator-logo',
+    companyName: 'Vectornator',
+    companyLocation: 'Berlin, Germany',
+    jobTitle: 'Senior product design',
+    jobDuration: 'Sep 2010 - Jul 2013',
   },
 ];
 
@@ -42,10 +43,10 @@ const ExperienceSection = () => {
   return (
     <>
       <h2>Experience</h2>
-      <ul>
+      <div className={styles.experienceSection}>
         {experienceInfo &&
           experienceInfo.map((exp) => <ExperienceCard {...exp} />)}
-      </ul>
+      </div>
     </>
   );
 };
