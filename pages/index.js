344b4adc7b4dcc7034049b8e220cd64a4ec50462
Mayank Sethi <mayank@digibrain.co.in>
2024-05-14 16:01:52 +0530

    add education section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import ExperienceSection from '@/components/experience/experienceSection';
 import SkillsSection from '@/components/skills/skillsSection';
+import EducationSection from '@/components/education/educationSection';
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <div className="col-8" style={{ padding: '50px' }}>
         <ExperienceSection />
         <SkillsSection />
+        <EducationSection />
       </div>
     </div>
   );
