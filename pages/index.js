ffb1f55df37d187135cceee02f4b3a98ad79cb62
Mayank Sethi <mayank@digibrain.co.in>
2024-05-14 19:47:00 +0530

    added profile section

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,16 @@ import ExperienceSection from '@/components/experience/experienceSection';
 import SkillsSection from '@/components/skills/skillsSection';
 import EducationSection from '@/components/education/educationSection';
 import ProjectSection from '@/components/projects/projectSection';
+import ProfileSection from '@/components/profile/profileSection';
+import styles from '@/styles/styles.module.scss';
 
 const Home = () => {
   return (
     <div className="row">
-      <div className="col-4"></div>
-      <div className="col-8" style={{ padding: '50px' }}>
+      <div className={`col-4 ${styles.mainProfileContainerBox}`}>
+        <ProfileSection />
+      </div>
+      <div className={`col-8 ${styles.mainContentContainerBox}`}>
         <ExperienceSection />
         <SkillsSection />
         <EducationSection />
