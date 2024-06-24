07d07a79f9b5d2fb8d6e5091dcf92f982bbefb2c
Mayank Sethi <mayank@digibrain.co.in>
2024-06-25 02:54:31 +0530

    first commit

--- components/skills/skillCard.jsx
+++
@@ -1,9 +0,0 @@
-import styles from '@/components/skills/styles.module.scss';
-
-const SkillCard = ({ skill }) => (
-  <div className={styles.skillCardBox} key={skill}>
-    <span>{skill}</span>
-  </div>
-);
-
-export default SkillCard;
